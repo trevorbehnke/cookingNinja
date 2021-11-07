1b7193eafa14a29b6d5cb4e2bd7e1576eba7172f
trevor <me@trevorbehnke.com>
2021-11-07 12:39:12 -0800

    feat: :lipstick: finished

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import "./App.css";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { useTheme } from "./hooks/useTheme";
+
+// page components
+import Navbar from "./components/Navbar";
 import Home from "./pages/home/Home";
 import Create from "./pages/create/Create";
-import Recipe from "./pages/recipe/Recipe";
 import Search from "./pages/search/Search";
-import Navbar from "./components/Navbar";
+import Recipe from "./pages/recipe/Recipe";
+import ThemeSelector from "./components/ThemeSelector";
+
+// styles
+import "./App.css";
 
 function App() {
+  const { mode } = useTheme();
+
   return (
-    <div className="App">
+    <div className={`App ${mode}`}>
       <BrowserRouter>
         <Navbar />
+        <ThemeSelector />
         <Switch>
           <Route exact path="/">
             <Home />
